a7d4266f386844f93da69725dd810090b9786b6e
Jesus <jesus@jesusyepes.com>
2021-11-04 12:47:57 +0100

    Update convertError.ts
    Unhandled Rejection (TypeError): Cannot convert undefined or null to object fixed

--- src/convertError.ts
+++ src/convertError.ts
@@ -6,10 +6,11 @@ export function convertError(errors: ValidationError[]) {
   const result: Ret = {};
 
   for (const error of Array.from(errors)) {
-    result[error.property] = Object.values(error.constraints!)[0];
-
     if (error.children.length > 0) {
       result[error.property] = convertError(error.children);
+    }
+    else {
+      result[error.property] = Object.values(error.constraints!)[0];
     }
   }
 
